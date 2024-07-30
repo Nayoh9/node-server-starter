a420e6c59cf6972009d0924d2455229b0a4cf864
Yohann Andre <y.andre90000@gmail.com>
2024-07-30 13:34:04 +0200

    change routing with index add new error middleware

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-const errorHandler = require('errorhandler');
+const routes = require("./routes/index");
+// const errorHandler = require('errorhandler');
 require("./database/index");
 const dotenv = require('dotenv').config()
-const { v4: uuidv4 } = require('uuid');
+
 
 
 
@@ -13,8 +14,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('short'));
-app.use(errorHandler);
+// app.use(errorHandler);
 app.use(express.json());
+
+app.use(routes);
 
 
 if (process.env.NODE_ENV === 'development') {
@@ -22,12 +25,33 @@ if (process.env.NODE_ENV === 'development') {
 } else {
     // Creation d'un middleware d'erreur
     app.use((err, req, res, next) => {
+
+        console.log(err);
+        // Validators error handling
+        if (err.errors) {
+            const errors = err.errors
+            const errorsArray = [];
+            for (const [key, value] of Object.entries(errors)) {
+                errorsArray.push(value.message);
+            }
+            return res.status(500).json({ error_messages: errorsArray });
+        }
+
+        // Duplicate document error handling
+        if (err.errorResponse && err.errorResponse.code === 11000) {
+            const keyName = Object.keys(err.errorResponse.keyValue);
+            return res.status(500).json(`Erreur de duplication : ${keyName}`);
+        }
+
+        // Single error handling
         const code = err.code || 500;
-        res.status(code).json({
+        return res.status(code).json({
             code: code,
             message: code === 500 ? null : err.message
         })
+
     });
+
 }
 
 console.log(process.env.NODE_ENV, "/", "port :", port);
